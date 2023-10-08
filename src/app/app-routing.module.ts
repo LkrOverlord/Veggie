560430b0dcba236a940780f667b635edcb4574a6
LkrOverlord <menesestapia@gmail.com>
2023-10-08 18:36:07 -0300

    Cambio faltante para el proyecto base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerDashboardComponent } from './pages/customer-dashboard/customer-dashboard.component';
+import { EmployeeDashboardComponent } from './pages/employee-dashboard/employee-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'customer', pathMatch: 'full' },
+  { path: 'customer', component: CustomerDashboardComponent },
+  { path: 'employee', component: EmployeeDashboardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
